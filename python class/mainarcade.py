85af7f9391389602d732744a6e54ffe778a3b948
TomasBL88 <tbakoslang@sympatico.ca>
2016-03-05 01:48:53 -0500

    Update to map class
    Map almost done

--- python class/mainarcade.py
+++ python class/mainarcade.py
@@ -12,25 +12,54 @@ GREEN=[0,255,0]
 BLUE =[0,0,255]
 BROWN = [139,69,19]
 
-SCREEN_DIMS = [840,525]
+SCREEN_DIMS = [1680,1050]
 SCALE_FACTOR = 0.5
 TILE_DIMS = [30,30]
-OBSTACLE_COLOUR=[GREEN, BROWN,PURPLE]
+OBSTACLE_PNG=["grasstile.png", "bricktile.png","fence.png", "tnttile.png"]
+TILE_TYPE=[0,1,2,3]
 
-class Obstacle(pygame.sprite.Sprite):
+immutable_object=pygame.sprite.Group()
+destructible_object=pygame.sprite.Group()
+bystander_object=pygame.sprite.Group()
+explosive_object=pygame.sprite.Group()
+
+
+
+class Tile(pygame.sprite.Sprite):
     """This is a class obstacle which is basically a wall"""
-    def __init__(self,x,y,TILE_DIMS,color,blocktype):
+    def __init__(self,tiletype):
         """constructor function"""
         super().__init__()
         
-        self.image = pygame.Surface(TILE_DIMS)
-        self.image.fill(OBSTACLE_COLOUR[blocktype])
-        self.rect=self.image.get_rect()
-        self.rect.y=y
-        self.rect.x=x
+        self.image=pygame.image.load(OBSTACLE_PNG[tiletype]).convert()
+        self.image.set_colorkey(WHITE)
+        
 
-        blocktype=0
-        self.blocktype=blocktype
+        self.tiletype=tiletype
+        self.rect=self.image.get_rect()
+
+class Map():
+    def __init__(self,tilelist):
+        self.tilelist=tilelist
+    def draw_map(self):
+        for tile in self.tilelist:
+            currenttile = Tile(self.tilelist[tile])
+            currenttile.rect.x=(tile%56)*30
+            currenttile.rect.y=tile/56*30
+            print (currenttile.rect.x)
+        
+            if(self.tilelist[tile]==0):
+                bystander_object.add(currenttile)
+            if(self.tilelist[tile]==1):
+                immutable_object.add(currenttile)
+            if(self.tilelist[tile]==2):
+                destructive_object.add(currenttile)
+            if(self.tilelist[tile]==3):
+                explosive_object.add(currenttile)
+
+            
+            
+            
         
         
 
@@ -44,13 +73,31 @@ clock=pygame.time.Clock()
 #Main Program Loop
 def main():
     done = False
+    screen.fill(GREY)
+    tilelist=[]
+    for i in range(0,1960):
+        tilelist.append(1)
+    backgroundmap = Map(tilelist)
+    backgroundmap.draw_map()
+
+    immutable_object.draw(screen)
+    destructible_object.draw(screen)
+    bystander_object.draw(screen)
+    explosive_object.draw(screen)
+   
+    
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-        screen.fill(GREY)
+       
+        
+
+        
+        
         pygame.display.flip()
         clock.tick(20)
+        
     pygame.quit()
 main()
 
