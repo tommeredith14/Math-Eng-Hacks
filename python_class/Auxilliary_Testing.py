52ccffec21d66617b1f492b13cff4c7fd4ac2fac
willdavidc <willdavidc@gmail.com>
2016-03-05 09:49:48 -0500

    Updated Auxiliary Testing

--- python_class/Auxilliary_Testing.py
+++ python_class/Auxilliary_Testing.py
@@ -1,7 +1,40 @@
 #!/usr/bin/python
 import serial
 import time
+import math
+import pygame
+import random
 ser = serial.Serial('/dev/ttyACM1', 9600)
+
+pygame.init()
+import Enemy_Bullet_Classes
+from Enemy_Bullet_Classes import *
+#pygame.mixer.music.load('Cave_Story')
+
+#Colours
+BLACK = [  0,  0,  0]
+GREY = [122, 79, 79]
+WHITE = [255,255,255]
+RED = [255,  0,  0]
+PURPLE = [153,0,153]
+GREEN=[0,255,0]
+BLUE =[0,0,255]
+BROWN = [139,69,19]
+
+SCREEN_DIMS = [1560,900]
+SCALE_FACTOR = 0.5
+TILE_DIMS = [30,30]
+OBSTACLE_PNG=["grasstile.png", "bricktile.png","fence.png", "tnttile.png","watertile.png","fire.png"]
+TILE_TYPE=[0,1,2,3,4,5]
+
+RESPONSE = 0.004
+
+immutable_object=pygame.sprite.Group()
+destructible_object=pygame.sprite.Group()
+bystander_object=pygame.sprite.Group()
+explosive_object=pygame.sprite.Group()
+drowning_object=pygame.sprite.Group()
+fire_object=pygame.sprite.Group()
 
 Input = [0, 0, 0, 0, 0, 0]
 print('Horizontal   Vertical  Shooting  Steering   Other   Pause')
@@ -31,29 +64,6 @@ def Update(Input): #UPDATES DATA READINGS
 #       time.sleep(1)print('Reading...')
 
 
-
-
-
-
-
-
-
-
-import pygame
-import random
-import math
-
-pygame.init()
-
-size = [1560, 900]
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Will Clark's Game")
-
-BLACK = (  0,   0,   0)
-WHITE = [255, 255, 255]
-RED   = (255,   0,   0)
-
-RESPONSE = 0.005
 
 clock = pygame.time.Clock()
 
@@ -193,33 +203,200 @@ class Tank(pygame.sprite.Sprite):
         rendering2.add(self.turret)
         rendering.draw(screen)
         rendering2.draw(screen)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Tile(pygame.sprite.Sprite):
+    """This is a class obstacle which is basically a wall"""
+    def __init__(self,tiletype):
+        """constructor function"""
+        super().__init__()
+        
+        self.image=pygame.image.load(OBSTACLE_PNG[tiletype]).convert()
+       
+        self.image.set_colorkey(WHITE)
+        self.image = pygame.transform.scale2x(self.image)
+
+        self.tiletype=tiletype
+        self.rect=self.image.get_rect()
+    def switchType(self, newType):
+        self.tiletype=newType
+        self.image=pygame.image.load(OBSTACLE_PNG[newType]).convert()
+        self.image = pygame.transform.scale2x(self.image)
+
+class Map():
+    def __init__(self,tilelist):
+        self.tilelist=tilelist
+    def draw_map(self):
+        for tile,value in enumerate(self.tilelist):
+            currenttile = Tile(self.tilelist[tile])
+            currenttile.rect.x=(tile%52)*30
+            currenttile.rect.y=(tile//52)*30
+            
+        
+            if(self.tilelist[tile]==0):
+                bystander_object.add(currenttile)
+            if(self.tilelist[tile]==1):
+                immutable_object.add(currenttile)
+            if(self.tilelist[tile]==2):
+                destructible_object.add(currenttile)
+            if(self.tilelist[tile]==3):
+                explosive_object.add(currenttile)
+            if(self.tilelist[tile]==4):
+                drowning_object.add(currenttile)
+            if(self.tilelist[tile]==5):
+                fire_object.add(currenttile)
+            
+    def update(self):
+        bystander_object.empty()
+        immutable_object.empty()
+        explosive_object.empty()
+        drowning_object.empty()
+        self.draw_map()
+            
+
+            
+            
+            
+        
+
+    
+
+    
         
 
 
 
+screen = pygame.display.set_mode(SCREEN_DIMS)#,pygame.FULLSCREEN)
+pygame.display.set_caption("ARCADE")
+clock=pygame.time.Clock()
 
-tank = Tank(14)
 
-immut = pygame.sprite.Group()
-destruct = pygame.sprite.Group()
+#Main Program Loop
+def main():
+    done = False
+    count = 0
+    screen.fill(GREY)
+    tilelist=[]
+    
+    def map_one(tilelist):
+        for i in range(0,1560):
+            tilelist.append(0)
+            if i<53 or i>1508 or i%52==0 or i%52==51:
+                tilelist[i]=1
+            elif i%52 == 5 and i<1000:
+                tilelist[i]=2
+            elif i%52 >20 and i%52<28 and i>500 and i<1000 :
+                tilelist[i]=4
+            elif i%52>20 and i<700 and i>599 :
+                tilelist[i]=4
+                if i%52 >40 and i%52 <44:
+                    tilelist[i]=2
+                elif i%52==40 or i%52==44:
+                    tilelist[i]=1
+            elif ((i%52==11) or (i%52==15)) and (i>400 and i<1200):
+                tilelist[i] =1
+            else:
+                num=random.randint(0,100)
+                if num>99:
+                    tilelist[i]=3
+    def map_two(tilelist):
+        for i in range(0,1560):
+            tilelist.append(0)
+            if (i<53 or i>1508 or i%52==0 or i%52==51) :
+                tilelist[i]=1
+            elif (i%52%3==0 and i%52%6==0) and not(i>730 and i<1000) and not (i%52==48):
+                tilelist[i]=1
+            elif (i>1040 and i<1092) or (i>676 and i<728) :
+                tilelist[i]=3
 
-done = False
-while done == False:
-    
-    screen.fill(WHITE)
-    
-    Update(Input)
-    
-    tank.inputFromController(Input[0], Input[1], Input[3])
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    
+    
+    map_one(tilelist)
+    backgroundmap = Map(tilelist)
+    backgroundmap.draw_map()
 
-    tank.update(immut, destruct)
-    tank.render()
+    #Collisions
+    def bullet_collisions(bullet_list,enemy_list):
+        hit_list=pygame.sprite.groupcollide(bullet_list,immutable_object,1,0)
+        hit_list=pygame.sprite.groupcollide(destructible_object,bullet_list,0,1)
+        for key in hit_list:
+            destructible_object.remove(key)
+            key.switchType(0)
+            bystander_object.add(key)
     
-    pygame.display.flip()
-    clock.tick(20)
+        hit_list=pygame.sprite.groupcollide(explosive_object,bullet_list,0,1)
+        for key in hit_list:
+            destructible_object.remove(key)
+            key.switchType(5)
+            bystander_object.add(key)
+
+        hit_list=pygame.sprite.groupcollide(enemy_list, bullet_list,0,1)
+        for key in hit_list:
+            key.do_damage(10)
+            
+            
+
+    bullet_list=pygame.sprite.Group()
+    enemy_list=pygame.sprite.Group()
+    enemy=Enemy(100,100,enemy_list,bullet_list,2)
+    count=0
+    enemy2=Enemy(300,100,enemy_list,bullet_list,4)
+   
     
-pygame.quit()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        if count%10==0:
+            enemy.fireBullet()
+        if count%7==0:
+            enemy2.fireBullet()
+
+        Update(Input)
+    
+        tank.inputFromController(Input[0], Input[1], Input[3])
+        
+            
+        bullet_list.update()
+        enemy_list.update()
+        bullet_collisions(bullet_list, enemy_list)
+        
+        bullet_list.draw(screen)
+        enemy_list.draw(screen)
+        
+        immutable_object.draw(screen)
+        destructible_object.draw(screen)
+        bystander_object.draw(screen)
+        explosive_object.draw(screen)
+        drowning_object.draw(screen)
+        bullet_list.draw(screen)
+        enemy_list.draw(screen)
+
+        
+        tank.update(immutable_object, destructible_object)
+        tank.render()
+        
+        count+=1
+        
+        
+        pygame.display.flip()
+        clock.tick(20)
+        
+    pygame.quit()
+main()
+
+    
+    
