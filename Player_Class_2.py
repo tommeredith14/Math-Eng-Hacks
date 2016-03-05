8b73eb03869d4d2775920ea7dd2c7a2bbd3c89c2
willdavidc <willdavidc@gmail.com>
2016-03-05 08:55:06 -0500

    Added control inputs

--- Player_Class_2.py
+++ Player_Class_2.py
@@ -1,3 +1,48 @@
+#!/usr/bin/python
+import serial
+import time
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+Input = [0, 0, 0, 0, 0, 0]
+print('Horizontal   Vertical  Shooting  Steering   Other   Pause')
+
+def Update(Input): #UPDATES DATA READINGS
+    ser.write(b'`')     #tells arduino it wants some readings
+    val1 = ser.readline().strip() #reads all input at once as a string
+    if (len(val1) != 0):          #makes sure not empty
+        rawArray = val1.decode().split(' ')    #generates array with ' ' delimiter
+        if (len(rawArray) == 6):           #gets rid of first garbage read
+            Input[0] = int(rawArray[0])
+            Input[1] = int(rawArray[1])
+            Input[2] = int(rawArray[2])
+            Input[3] = str(rawArray[3])
+            Input[4] = int(rawArray[4])
+            Input[5] = int(rawArray[5])
+            return
+
+def PrintList(Input):
+    print(Input[0], '\t', Input[1])
+    return
+
+
+
+
+#val1 = ser.readline().strip() #reads all input at once as a string
+#if (len(val1) != 0):          #makes sure not empty
+#    testArray = str(val1).split(' ')    #generates array with ' ' delimiter
+#    if (len(testArray) == 6):           #gets
+#        print (testArray)
+#       time.sleep(1)print('Reading...')
+
+
+
+
+
+
+
+
+
+
 import pygame
 import random
 import math
@@ -43,17 +88,17 @@ class Tank(pygame.sprite.Sprite):
         self.still_alive = True # It was a triumph.
 
 # Sets up the image and Rect
-        self.image = pygame.image.load("TestPic.png").convert_alpha()
+        self.image = pygame.image.load("Chassis.png").convert_alpha()
         self.image.set_colorkey((255,255,255))
         self.rect = self.image.get_rect()
         self.rect.centerx = size[0] / 2
         self.rect.centery = size[1] / 2
         self.image2 = self.image
         self.angle = 0
-        self.speedx = 1.0
-        self.speedy = 1.0
-        self.accelx = 2.0
-        self.accely = -1.0
+        self.speedx = -600
+        self.speedy = 300
+        self.accelx = 78
+        self.accely = -38.0
         self.bullet_list = bullet_list
 
         self.turret = Turret()
@@ -69,7 +114,12 @@ class Tank(pygame.sprite.Sprite):
     def inputFromController(self, xjoy, yjoy, turret_rot):
         self.accelx = xjoy
         self.accely = yjoy
-        self.turret_rot = turret_rot
+        if (turret.rot == "L"):
+            self.turret_rot = 1
+        elif (turret.rot == "R"):
+            self.turret_rot = -1
+        else:
+            self.turret_rot = 0
 
 
     def collideImmutable(self, immutable_list):
@@ -108,7 +158,7 @@ class Tank(pygame.sprite.Sprite):
             
  #           self.turret_angle += 30
             
-            self.angle -=10
+ #           self.angle -=10
             
             oldxpos = self.rect.centerx
             oldypos = self.rect.centery
@@ -144,7 +194,7 @@ class Tank(pygame.sprite.Sprite):
         rendering.add(self)
         rendering2.add(self.turret)
         rendering.draw(screen)
-        #rendering2.draw(screen)
+        rendering2.draw(screen)
         
 
 
@@ -159,6 +209,10 @@ done = False
 while done == False:
     
     screen.fill(WHITE)
+    
+    def Update(Input):
+    
+    tank.inputFromController(Input[0], Input[1], Input[3])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
