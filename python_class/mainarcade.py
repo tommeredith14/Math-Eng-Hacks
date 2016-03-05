9685b8eb942e6a62c9162d7c80f27b9777377ef2
TOM MEREDITH <tommeredith14@gmail.com>
2016-03-05 14:39:41 -0500

    Merged in Healthbar to main

--- python_class/mainarcade.py
+++ python_class/mainarcade.py
@@ -27,6 +27,70 @@ def Update(Input): #UPDATES DATA READINGS
             return
 
 
+import RPi.GPIO as GPIO
+LEDS = [7, 11, 12, 13, 15, 16, 18, 22] #starts at red to green
+Buzzer = 29
+HealthLevel = 8
+
+def SetLights(level):
+    #fixes wrong levels
+    if (level > 8):
+        level = 8
+    if (level < 0):
+        level = 0
+        
+    #turn off all lights above level
+    for i in range(7, level-1, -1):
+        GPIO.output(LEDS[i], GPIO.LOW)
+    #turn on all lights at and below level
+    for i in range(level):
+        GPIO.output(LEDS[i], GPIO.HIGH)
+
+def TurnOff():
+    for i in range(8):
+        GPIO.output(LEDS[i], GPIO.LOW)
+    GPIO.output(Buzzer, GPIO.LOW)
+
+        
+GPIO.setmode(GPIO.BOARD) # Number GPIOs by its physical location
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, GPIO.LOW)
+GPIO.setup(11, GPIO.OUT)
+GPIO.output(11, GPIO.LOW)
+GPIO.setup(12, GPIO.OUT)
+GPIO.output(12, GPIO.LOW)
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.LOW)
+GPIO.setup(15, GPIO.OUT)
+GPIO.output(15, GPIO.LOW)
+GPIO.setup(16, GPIO.OUT)
+GPIO.output(16, GPIO.LOW)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, GPIO.LOW)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(22, GPIO.LOW)
+GPIO.setup(29, GPIO.OUT)
+GPIO.output(29, GPIO.LOW)
+
+#testing
+SetLights(HealthLevel)
+GPIO.output(Buzzer, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(Buzzer, GPIO.LOW)
+HealthLevel = 1
+SetLights(HealthLevel)
+
+            
+#time.sleep(2)
+#SetLights(3)
+#time.sleep(2)
+#SetLights(12)
+#time.sleep(2)
+#SetLights(-4)
+#time.sleep(2)
+
+
+
 #Colours
 BLACK = [  0,  0,  0]
 GREY = [122, 79, 79]
@@ -311,10 +375,16 @@ def main():
         bullet_list.draw(screen)
         enemy_list.draw(screen)
 
+
+        SetLights(math.ceil(8* (player.health / 1000)))
+
         player.react()
         player.render(screen)
         
         count+=1
+        pygame.draw.rect(screen, RED, (650, 45, 200 * (player.health / 1000), 30))
+        pygame.draw.rect(screen, RED, (650, 45, 200, 30), 5)
+        
         
         
         pygame.display.flip()
