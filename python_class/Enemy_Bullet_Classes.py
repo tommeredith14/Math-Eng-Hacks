fe8fb775d5ae183c987f03751d3db0dbe3d06bbf
TOM MEREDITH <tommeredith14@gmail.com>
2016-03-05 10:19:50 -0500

    Fix git

--- python_class/Enemy_Bullet_Classes.py
+++ python_class/Enemy_Bullet_Classes.py
@@ -30,30 +30,35 @@ class Enemy(pygame.sprite.Sprite):
     def fireBullet(self, speed = None, angle = None):
         # newBullet = Bullet(self.hitbox.rect.x, self.hitbox.rect.y, 1)
         if self.health > 0:
+            print("fire")
             if speed == None:
                 speed = random.uniform(self.speed+1,7)
             if angle == None:
                 angle = random.uniform(-math.pi,math.pi)
-            newBullet = Bullet(speed,self.rect.x, self.rect.y,angle, self.list_to_fire)
+            newBullet = Bullet(speed,self.rect.x, self.rect.y,angle, self.list_to_fire,self)
            #  bullet_list.add(newBullet)
     def do_damage(self, damage):
         self.health -= damage
         if self.health <= 0:
             self.enemy_list.remove(self)
             self.remove()
+    def getType(self):
+        return "Enemy"
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, speed, x_pos, y_pos, angle, bullet_list):
+    def __init__(self, speed, x_pos, y_pos, angle, bullet_list,shooter):
         super().__init__()
         self.image = pygame.image.load("bullet.png").convert()
         self.image.set_colorkey(WHITE)
         self.image = pygame.transform.scale2x(self.image)
         self.rect = self.image.get_rect()
-        self.xSpeed = speed * math.cos(angle)
+        self.xSpeed = speed * math.cos(angle) 
         self.ySpeed = speed * math.sin(angle)
-        self.rect.x = x_pos
-        self.rect.y = y_pos
+        self.rect.x = x_pos + self.xSpeed *  1
+        self.rect.y = y_pos + self.ySpeed *  1
         bullet_list.add(self)
+        self.sType = shooter.getType()
+        print("Success")
 
     def update(self):
         self.rect.x += self.xSpeed *  1
